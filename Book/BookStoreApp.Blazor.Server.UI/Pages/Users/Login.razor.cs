6064ca29890d391ad2e86151a330ebd97b6ebeb6
Simone <simone@healthtrack.com.au>
2022-06-06 10:50:21 +1000

    UI & ARCHITECHTURAL CONSIDERATION Code restructured slightly. Section 12 completed.

---
+++ Book/BookStoreApp.Blazor.Server.UI/Pages/Users/Login.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using BookStoreApp.Blazor.Server.UI.Services.Authentication;
+using BookStoreApp.Blazor.Server.UI.Services.Base;
+using System;
+
+namespace BookStoreApp.Blazor.Server.UI.Pages.Users
+{
+    public partial class Login
+    {
+        [Inject] private IAuthenticationService authService { get; set; }
+        [Inject] private NavigationManager navManager { get; set; }
+
+        LoginUserDto LoginModel = new LoginUserDto();
+        string message = string.Empty;
+
+        private async Task HandleLogin()
+        {
+            var response = await authService.AuthenticateAsync(LoginModel);
+
+            if (response.Success)
+            {
+                navManager.NavigateTo("/");
+            }
+
+            message = response.Message;
+        }
+    }
+}
