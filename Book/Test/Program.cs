f119d08f11fe0a9f8504290c6090f9a72096eb0d
Simone <simone@healthtrack.com.au>
2022-06-15 15:29:10 +1000

    COMMITTING CURRENT VERSION TO GO TO OLD VERSION Issues with packages not being detected(?). Seeing if old version will work.

---
+++ Book/Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DL;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var modelAssembly = typeof(NorthwindDataContext).Assembly;
+            var resourceStream =
+                modelAssembly.GetManifestResourceStream("Northwind.Tutorial.NorthwindMappings.xml");
+            return new NorthwindDataContext(XmlMappingSource.FromStream(resourceStream));
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
